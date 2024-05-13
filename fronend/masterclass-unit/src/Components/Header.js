e2d7ef1dcf71871de4b6f96553ae63595c137b4e
Andre <andre.coelho@hotmail.fr>
2024-05-13 15:40:58 +0200

    Add Header & Footer

---
+++ fronend/masterclass-unit/src/Components/Header.js
@@ -0,0 +1,13 @@
+import './Header.css';
+
+export default function Header() {
+    return (
+        <header className="WholeHeader">
+            <ul>
+                <li>Accueil</li>
+                <li>Liste des événements</li>
+                <li>Profil</li>
+            </ul>
+        </header>
+    )
+}

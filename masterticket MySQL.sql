dd774284946fb8d04db2b3162231269a8a548f7a
Tisma <matisagr@gmail.com>
2024-05-14 14:12:08 +0200

    chore: Update masterticket database schema and tables

--- masterticket MySQL.sql
+++ masterticket MySQL.sql
@@ -28,8 +28,6 @@ CREATE TABLE IF NOT EXISTS `masterticket`.`users` (
   `user_birth` DATE NOT NULL,
   `user_role` ENUM('user', 'admin') NOT NULL,
   PRIMARY KEY (`user_id`),
-  UNIQUE INDEX `user_mail_UNIQUE` (`user_mail` ASC) VISIBLE,
-  UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -40,7 +38,6 @@ CREATE TABLE IF NOT EXISTS `masterticket`.`categories` (
   `category_id` INT NOT NULL AUTO_INCREMENT,
   `categories_name` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`category_id`),
-  UNIQUE INDEX `category_id_UNIQUE` (`category_id` ASC) VISIBLE)
 ENGINE = InnoDB;
 
 
@@ -57,14 +54,6 @@ CREATE TABLE IF NOT EXISTS `masterticket`.`events` (
   `event_ageneed` INT NOT NULL,
   `event_category_id` INT NOT NULL,
   PRIMARY KEY (`event_id`),
-  INDEX `event_category_id_idx` (`event_category_id` ASC) VISIBLE,
-  UNIQUE INDEX `event_id_UNIQUE` (`event_id` ASC) VISIBLE,
-  UNIQUE INDEX `event_name_UNIQUE` (`event_name` ASC) VISIBLE,
-  CONSTRAINT `event_category_id`
-    FOREIGN KEY (`event_category_id`)
-    REFERENCES `masterticket`.`categories` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -77,19 +66,11 @@ CREATE TABLE IF NOT EXISTS `masterticket`.`invoices` (
   `invoice_event_id` VARCHAR(64) NOT NULL,
   `invoice_date` DATETIME NOT NULL,
   PRIMARY KEY (`invoice_id`),
+
+  
   INDEX `invoice_user_id_idx` (`invoice_user_id` ASC) VISIBLE,
   INDEX `invoice_event_id_idx` (`invoice_event_id` ASC) VISIBLE,
   UNIQUE INDEX `invoice_id_UNIQUE` (`invoice_id` ASC) VISIBLE,
-  CONSTRAINT `invoice_user_id`
-    FOREIGN KEY (`invoice_user_id`)
-    REFERENCES `masterticket`.`users` (`user_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `invoice_event_id`
-    FOREIGN KEY (`invoice_event_id`)
-    REFERENCES `masterticket`.`events` (`event_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
@@ -102,19 +83,6 @@ CREATE TABLE IF NOT EXISTS `masterticket`.`cancel` (
   `cancel_invoice_id` VARCHAR(64) NOT NULL,
   `cancel_reason` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`cancel_id`),
-  INDEX `cancel_invoice_id_idx` (`cancel_invoice_id` ASC) VISIBLE,
-  INDEX `cancel_event_id_idx` (`cancel_event_id` ASC) VISIBLE,
-  UNIQUE INDEX `cancel_id_UNIQUE` (`cancel_id` ASC) VISIBLE,
-  CONSTRAINT `cancel_event_id`
-    FOREIGN KEY (`cancel_event_id`)
-    REFERENCES `masterticket`.`events` (`event_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `cancel_invoice_id`
-    FOREIGN KEY (`cancel_invoice_id`)
-    REFERENCES `masterticket`.`invoices` (`invoice_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 

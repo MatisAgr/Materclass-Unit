33592290f66e35b7e496b98c9cd769873ba24b92
methiathibault <methiathibault@gmail.com>
2024-05-15 18:50:45 +0200

    adding password_hashind, exception to methods, and all kind of test

--- backend/tests/Unit/UsersTest.php
+++ backend/tests/Unit/UsersTest.php
@@ -19,9 +19,149 @@ class UsersTest extends TestCase
     {
         $this->users->createUser('Test User', 'testuser@test.com', 'password', '2000-01-01', 'user');
 
-        $stmt = $this->users->getUserId('1');
+        $stmt = $this->users->getUserId(1);
 
         $this->assertEquals('Test User', $stmt['user_username']);
     }
     
+    public function testCreateUserWithInvalidEmail()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid email address');
+        
+        $this->users->createUser('Test User', 'testuser', 'password', '2000-01-01', 'user');
+    }
+
+    public function testCreateUserWithInvalidPassword()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('All fields are required');
+        
+        $this->users->createUser('Test User', 'testuser@test.com', NULL, '2000-01-01', 'user');
+    }
+
+    public function testCreateUserWithInvalidBirthDate()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid birth date');
+        
+        $this->users->createUser('Test User', 'testuser@test.com', 'password', '2000-01-32', 'user');
+    }
+
+    public function testCreateUserWithInvalidRole()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid role');
+        
+        $this->users->createUser('Test User', 'testuser@test.com', 'password', '2000-01-01', 'invalid');
+    }
+
+    public function testCreateUserWithExistingEmail()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Email already exists');
+        
+        $this->users->createUser('Test User', 'testuser@test.com', 'password', '2000-01-01', 'user');
+    }
+
+    public function testGetUserId()
+    {
+        $stmt = $this->users->getUserId(1);
+
+        $this->assertEquals('Test User', $stmt['user_username']);
+    }
+
+    public function testGetUserIdWithInvalidId()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid user ID');
+        
+        $this->users->getUserId('0');
+    }
+
+    public function testGetUserByEmail()
+    {
+        $stmt = $this->users->getUserByEmail('testuser@test.com');
+
+        $this->assertEquals('Test User', $stmt['user_username']);
+    }
+
+    public function testGetUserByEmailWithInvalidEmail()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid email address');
+        
+        $this->users->getUserByEmail('testuser');
+    }
+
+    public function testGetUserByEmailAndPassword()
+    {
+        $email = 'testuser@test.com';
+
+        $user = $this->users->getUserByEmailAndPassword($email, 'password');
+
+        $this->assertEquals($email, $user['user_mail']);
+        $this->assertEquals('Test User', $user['user_username']);
+    }
+
+    public function testGetUserByEmailAndPasswordWithInvalidEmail()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid email address');
+        
+        $this->users->getUserByEmailAndPassword('testuser', 'password');
+    }
+
+    public function testGetUserByEmailAndPasswordWithInvalidPassword()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('All fields are required');
+        
+        $this->users->getUserByEmailAndPassword('testuser@test.com', NULL);
+    }
+
+    public function testGetAllUsers()
+    {
+        $stmt = $this->users->getAllUsers();
+
+        $this->assertIsArray($stmt);
+    }
+
+    public function testDeleteUser()
+    {
+        $this->users->deleteUser(1);
+
+        $stmt = $this->users->getUserId(1);
+
+        $this->assertEmpty($stmt);
+    }
+
+    public function testDeleteUserWithInvalidId()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid user ID');
+        
+        $this->users->deleteUser('0');
+    }
+
+    public function getAllEventsForUser()
+    {
+        $stmt = $this->users->getAllEventsForUser(1);
+
+        $this->assertIsArray($stmt);
+    }
+
+    public function testGetAllEventsForUserWithInvalidId()
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage('Invalid user ID');
+        
+        $this->users->getAllEventsForUser('0');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->db = null;
+        $this->users = null;
+    }
 }

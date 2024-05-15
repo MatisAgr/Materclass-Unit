46f96ff99240c5fa08f76bf0c38bfd79dc49cce6
methiathibault <methiathibault@gmail.com>
2024-05-15 11:03:46 +0200

    CategoryTest finished

--- backend/tests/Unit/CategoryTest.php
+++ backend/tests/Unit/CategoryTest.php
@@ -1,119 +1,54 @@
 <?php
-require_once 'models/Category.php'; 
-require_once 'models/Database.php'; 
-
 use Masterticket\Category;
 use Masterticket\Database;
 use PHPUnit\Framework\TestCase;
 
 class CategoryTest extends TestCase {
-    public function testCreateEvent() {
-        $dbModelMock = $this->getMockBuilder(Database::class)
-                            ->getMock();
+    private $db;
+    private $category;
 
-        $category = new Category($dbModelMock);
-        $category_name = "Test Category";
-
-        $pdoStatementMock = $this->getMockBuilder(PDOStatement::class)
-                                 ->disableOriginalConstructor()
-                                 ->getMock();
-
-        $pdoStatementMock->expects($this->once())
-                         ->method('bindValue')
-                         ->with(':col1', $category_name, PDO::PARAM_STR);
-
-        $pdoStatementMock->expects($this->once())
-                         ->method('execute');
-
-        $pdoMock = $this->getMockBuilder(PDO::class)
-                        ->disableOriginalConstructor()
-                        ->getMock();
-
-        $pdoMock->expects($this->once())
-                ->method('prepare')
-                ->willReturn($pdoStatementMock);
-
-        $dbModelMock->expects($this->once())
-                    ->method('connect')
-                    ->willReturn($pdoMock);
-
-        $category->createEvent($category_name);
+    protected function setUp(): void {
+        $this->db = new Database();
+        $this->db = $this->db->connect();
+        $this->category = new Category();
     }
 
-    public function testGetEventById() {
-            $dbModelMock = $this->getMockBuilder(Database::class)
-                                ->getMock();
-    
-            $category = new Category($dbModelMock);
-    
-            $category_id = 1;
-    
-            $pdoStatementMock = $this->getMockBuilder(PDOStatement::class)
-                                     ->disableOriginalConstructor()
-                                     ->getMock();
-    
-            $pdoStatementMock->expects($this->once())
-                             ->method('bindValue')
-                             ->with(':col1', $category_id, PDO::PARAM_STR);
-    
-            $fetchResult = ['category_id' => $category_id, 'category_name' => 'Test Category']; // Simuler les données retournées par fetch()
-            $pdoStatementMock->expects($this->once())
-                             ->method('fetch')
-                             ->willReturn($fetchResult);
-    
-            $pdoMock = $this->getMockBuilder(PDO::class)
-                            ->disableOriginalConstructor()
-                            ->getMock();
-    
-            $pdoMock->expects($this->once())
-                    ->method('prepare')
-                    ->willReturn($pdoStatementMock);
-    
-            $dbModelMock->expects($this->once())
-                        ->method('connect')
-                        ->willReturn($pdoMock);
-    
-            $result = $category->getEventById($category_id);
-            $this->assertEquals($fetchResult, $result);
+    public function testCreateCategory() {
+        $this->category->createCategory('Test Category');
+
+        $stmt = $this->category->getCategoryByName('Test Category');
+
+        $this->assertEquals('Test Category', $stmt['category_name']);
     }
 
-    public function testGetAllEvents() {
-        $dbModelMock = $this->getMockBuilder(Database::class)
-                            ->getMock();
+    public function testGetCategoryById() {
+        $stmt = $this->category->getCategoryById(1);
 
-        $category = new Category($dbModelMock);
+        $this->assertEquals('Test', $stmt['category_name']);
+    }
 
-        $mockedResults = [
-            ['event_id' => 1, 'event_name' => 'Event 1'],
-            ['event_id' => 2, 'event_name' => 'Event 2']
-        ];
+    public function testGetCategoryByName() {
+        $stmt = $this->category->getCategoryByName('Test');
 
-        $pdoStatementMock = $this->getMockBuilder(PDOStatement::class)
-                                 ->disableOriginalConstructor()
-                                 ->getMock();
+        $this->assertEquals(1, $stmt['category_id']);
+    }
 
-        $pdoStatementMock->expects($this->once())
-                         ->method('execute');
+    public function testGetAllCategory() {
+        $stmt = $this->category->getAllCategory();
 
-        $pdoStatementMock->expects($this->once())
-                         ->method('fetchAll')
-                         ->willReturn($mockedResults);
+        $this->assertIsArray($stmt);
+    }
 
-        $pdoMock = $this->getMockBuilder(PDO::class)
-                        ->disableOriginalConstructor()
-                        ->getMock();
+    public function testUpdateCategory() {
+        $this->category->updateCategory(1, 'Test');
 
-        $pdoMock->expects($this->once())
-                ->method('prepare')
-                ->willReturn($pdoStatementMock);
+        $stmt = $this->category->getCategoryById(1);
 
-        $dbModelMock->expects($this->once())
-                    ->method('connect')
-                    ->willReturn($pdoMock);
-
-        $result = $category->getAllEvents();
-
-        $this->assertEquals($mockedResults, $result);
+        $this->assertEquals('Test', $stmt['category_name']);
+    }
+    
+    protected function tearDown(): void {
+        $this->db = null;
+        $this->category = null;
     }
 }
-
